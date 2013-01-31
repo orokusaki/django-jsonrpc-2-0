914cd1167cf40702e7fbb0e2fffeab08370808b1
Michael Angeletti <michael@angelettigroup.com>
2013-01-30 20:20:56 -0500

    Added support for encoding ``Model`` and ``QuerySet`` objects

--- jsonrpc/encoders.py
+++ jsonrpc/encoders.py
@@ -2,20 +2,31 @@ import json
 import decimal
 from datetime import datetime, date, time
 
+from django.db import models
+from django.db.models.query import QuerySet
+from django.core.serializers import serialize
 from django.utils.functional import Promise
 from django.utils.encoding import force_unicode
 
 
 class RobustEncoder(json.JSONEncoder):
     """
-    JSON encoder with support for ``Promise``, ``datetime``, ``date``,
-    ``time``, and ``Decimal`` objects.
+    JSON encoder with support for ``QuerySet``, ``Model``, ``Promise``,
+    ``datetime``, ``date``, ``time``, and ``Decimal`` objects.
     """
     def default(self, obj):
         """
         Provides custom functionality for certain types, defaulting to the
         built-in encoder.
         """
+        # QuerySet
+        if isinstance(obj, QuerySet):
+            return json.loads(serialize('json', obj))
+
+        # Model
+        if isinstance(obj, models.Model):
+            return json.loads(serialize('json', [obj]))[0]
+
         # Promise (e.g., ``ugettext_lazy``), and Decimal both get unicoded
         if isinstance(obj, (Promise, decimal.Decimal)):
             return force_unicode(obj)
