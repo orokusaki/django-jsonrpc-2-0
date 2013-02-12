bce1010b6989a094a1df8c0da968fe3f8ec128cb
Michael Angeletti <michael@angelettigroup.com>
2013-02-11 22:14:26 -0500

    Fixed setup.py formatting issues

--- setup.py
+++ setup.py
@@ -5,15 +5,13 @@ from setuptools import setup, find_packages
 version = '.'.join([str(v) for v in __import__('jsonrpc').VERSION])
 
 setup(
-    name = 'jsonrpc',
-    description = (
-        'A JSON-RPC 2.0 server that is loosely coupled to Django'
-    ),
-    version = version,
-    author = 'Michael Angeletti',
-    author_email = 'michael [at] angelettigroup [dot] com',
-    url = 'http://github.com/orokusaki/django-jsonrpc-2-0/',
-    classifiers = [
+    name='jsonrpc',
+    description='A JSON-RPC 2.0 server that is loosely coupled to Django',
+    version=version,
+    author='Michael Angeletti',
+    author_email='michael [at] angelettigroup [dot] com',
+    url='http://github.com/orokusaki/django-jsonrpc-2-0/',
+    classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
